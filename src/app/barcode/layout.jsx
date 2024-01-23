e28dbd049de58dea83f13cd291376f4a82a2eb8c
Saad Ishfaq <sishfaq984@gmail.com>
2024-01-23 17:36:15 +0500

    Error fixed

--- src/app/barcode/layout.jsx
+++ src/app/barcode/layout.jsx
@@ -1,12 +1,9 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import Navbar from '../Navbar';
 
 export default function layout({children}) {
-    let path=usePathname()
  
     return (
         <>
